2e2901b129ae51886f9a43cb21f4fb6b3c2eab9c
Pablo <124668660+pabaguilar@users.noreply.github.com>
2024-12-13 19:24:23 +0100

    push

--- models/evento_schema.py
+++ models/evento_schema.py
@@ -4,11 +4,10 @@ from typing import List
 from typing import Literal
 
 class eventoSchema(BaseModel):
-    nombre: str = Field(...)
     timestamp: datetime = Field(default_factory=lambda:datetime.now(timezone(timedelta(hours=2))), description="Fecha de evento")
     lugar: str = Field(..., max_length=200,description="Lugar.")
     lat: float = Field(...,description="Latitud.")
     lon: float = Field(...,description="Longitud.")
-    organizador: str = Field(...,description="Email del organizador.")
+    email: str = Field(...,description="Email del organizador.")
     imagen: str = Field(...,description="URL de imagen")
 
--- routes/evento_route.py
+++ routes/evento_route.py
@@ -49,51 +49,18 @@ async def get_by_id(id: str):
         raise HTTPException(status_code=500, detail="Failed to retrieve")
 
 
-# -------------------------------- ALTERNATIVOS --------------------------------------------------
-
-# @router.put("/{email}/update")
-# async def put_by_(email: str,input: primerSchema = Body(...)):
-#     try:
-#         result = await primer_logic.put_by(email,input.model_dump())
-#         return result
-#     except Exception as e:
-#         print(f"Failed to retrieve entry: {str(e)}")
-#         raise HTTPException(status_code=500, detail="Failed to update")
-
-# @router.delete("/{email}")
-# async def delete_by_(email: str):
-#     try:
-#         deleted = await primer_logic.delete_by(email)
-#         return deleted
-#     except Exception as e:
-#         print(f"Failed to delete entry: {str(e)}")
-#         raise HTTPException(status_code=500, detail="Failed to delete")
-
-# @router.get("/{email}")
-# async def get_by_(email: str):
-#     try:
-#         result = await primer_logic.get_secundario(email)
-#         return result
-#     except Exception as e:
-#         print(f"Failed to retrieve entry: {str(e)}")
-#         raise HTTPException(status_code=500, detail="Failed to retrieve")
-
-# ------------------------------------ FIN ALTERNATIVOS -------------------------------------------
 
 
 @router.get("/")
 async def get_all(
-    nombre: Optional[str] =  Query(None),
-    organizador: Optional[str] = Query(None),
+    email: Optional[str] = Query(None),
     lat: Optional[float] = Query(None),
     lon: Optional[float] = Query(None)
 ):
     try:
         filter = {}
-        if nombre:
-            filter["nombre"] = {"$regex": ".*{}.*".format(nombre), "$options": "i"}
-        if organizador:
-            filter["organizador"] = {"$regex": ".*{}.*".format(organizador), "$options": "i"}
+        if email:
+            filter["email"] = {"$regex": ".*{}.*".format(email), "$options": "i"}
         if lat is not None:
             filter["lat"] = {"$gte": lat - 0.2, "$lte": lat + 0.2}
         if lon is not None:
